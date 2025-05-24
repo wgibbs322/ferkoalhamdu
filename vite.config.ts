f70b5dbe948819fa05e53663229e23d6ff249dc5
wgibbs322 <wgibbs322@gmail.com>
2025-05-24 00:28:15 -0700

    m

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.VITE_PORT) || 3000,
+    port: Number(process.env.PORT) || 3000, // Correct env variable for Render
   },
-}); 
+});
