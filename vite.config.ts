94fcb7fb605e017ea9b80fbde77b3ccb1e2a228a
wgibbs322 <wgibbs322@gmail.com>
2025-05-24 00:18:08 -0700

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.VITE_PORT) || 3000,
   },
-});
+}); 
